aec611376f9cbf1867f46de9a2d99cdca762e774
NactEunS <eweiyan05947809@126.com>
2021-06-10 22:41:45 +0800

    组员新建文件

---
+++ GZWeibo007/GZWeibo007/Class/Module/Home/View/Cell/Forward/CZStatusDetailJuHuaCell.swift
@@ -0,0 +1,24 @@
+//
+//  CZStatusDetailJuHuaCell.swift
+//  GZWeibo007
+//
+//  Created by Apple on 15/12/26.
+//  Copyright © 2015年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class CZStatusDetailJuHuaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
